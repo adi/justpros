c16222137c5d693124de15e813002ffe03ce6bb1
Adrian Punga <adrian.punga@gmail.com>
2025-11-29 12:45:44 +0200

    remove harmless warning

--- app/main.py
+++ app/main.py
@@ -10,6 +10,11 @@ app = FastAPI(title="JustPros")
 app.mount("/static", StaticFiles(directory=BASE_DIR / "static"), name="static")
 
 
+@app.get("/.well-known/appspecific/com.chrome.devtools.json")
+async def chrome_devtools():
+    return {}
+
+
 @app.get("/", response_class=HTMLResponse)
 async def root() -> str:
     return (BASE_DIR / "app" / "templates" / "index.html").read_text()
