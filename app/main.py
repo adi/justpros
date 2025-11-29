be19fa3b0617869c7eecdf4f5b63b040656b7e1d
Adrian Punga <adrian.punga@gmail.com>
2025-11-29 12:15:38 +0200

    add first html template

--- app/main.py
+++ app/main.py
@@ -1,9 +1,15 @@
+from pathlib import Path
+
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 app = FastAPI(title="JustPros")
+app.mount("/static", StaticFiles(directory=BASE_DIR / "static"), name="static")
 
 
 @app.get("/", response_class=HTMLResponse)
 async def root() -> str:
-    return "JustPros"
+    return (BASE_DIR / "app" / "templates" / "index.html").read_text()
